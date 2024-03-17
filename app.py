9f6a9a661ff9b61e28b808e29c658a89f3e148e5
naroamartin <133049952+naroamartin@users.noreply.github.com>
2024-03-17 23:15:31 +0000

    diferentes html

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__) 
 
+
 @app.route("/")
 
-def hello_world():
+def home():
   return render_template ("home.html")
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+  
+@app.route('/delete')
+def delete():
+      return render_template('delete.html')
+  
+@app.route('/edit')
+def edit():
+      return render_template('edit.html')
+  
+@app.route('/info')
+def info():
+    return render_template('info.html')
+  
 if __name__ == '__main__':        
   app.run(host='0.0.0.0', debug=True)
